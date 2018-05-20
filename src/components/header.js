c13f120a16d1f84ae7bed5b090996e8c10f4f2cd
antcin <ciniglioantonio@gmail.com>
2018-05-20 22:48:44 +0100

    style header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,34 @@
 // import libraries for making component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 // make component
 const Header = () => {
-  return <Text>Albums!</Text>;
+  const { textStyle, viewStyle } = styles; //destructuring to remove duplicate references to styles
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 // make component available to other parts of app
